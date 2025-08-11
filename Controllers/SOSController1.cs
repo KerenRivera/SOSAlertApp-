1aaef4b74aa9677a82588bf2dbe53803904aae15
Keren <131375714+KerenRivera@users.noreply.github.com>
2025-08-11 16:15:14 -0400

    Primer controller

---
+++ Controllers/SOSController1.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SOS_Alert_App.Data;
+using Microsoft.EntityFrameworkCore;
+using SOS_Alert_App.Models;
+
+namespace SOS_Alert_App.Controllers
+{
+    public class SOSController1 : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public SOSController1(AppDbContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            var contacts = _context.Contacts.ToList();
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult SendAlert(int contactId, string? location = null)
+        {
+            var contact = _context.Contacts.Find(contactId);
+            if (contact == null)
+            { 
+                return NotFound("Contacto no encontrado.");
+            }
+
+            var message = "¡Alerta SOS! Estoy en una situación de emergencia. Mi ubicación es: " + (location);
+            if (string.IsNullOrEmpty(location))
+            {
+                message += $" Location: {location}";
+            }
+
+            var alert = new AlertLog
+            {
+                ContactId = contactId,
+                Message = message,
+                SentAt = DateTime.Now
+            };
+
+            _context.AlertLogs.Add(alert);
+            _context.SaveChanges();
+
+            TempData["Success"] = $"Alert sent to {contact.Name} successfully.";
+            return RedirectToAction(nameof(Index));
+        }
+
+    }
+}
